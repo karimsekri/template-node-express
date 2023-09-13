eee4725d09a7b1983d03d6e6876b39fa2c3f59d1
karim <karim.sekri@gmail.com>
2023-09-13 10:38:55 +0200

    Ajout de la route hello

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+
+const app = express();
+dotenv.config();
 console.log("Hello");
+const myport = process.env.PORT ? parseInt(process.env.PORT as string) : 3030;
+
+app.get("/hello", async (_, res) => {   
+        
+    res.send("Hello");   
+});
+
+app.listen( myport, () =>
+  console.log(`Server is listening on port ${myport}`)
+);
